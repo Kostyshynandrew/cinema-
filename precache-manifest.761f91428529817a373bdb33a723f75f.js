515c85dbf7daafb14756e818c0e4adc96509ed6b
Andrii <kostyshynandrew@gmail.com>
2019-01-10 17:03:26 +0200

    Updates

---
+++ precache-manifest.761f91428529817a373bdb33a723f75f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "72601ca0ea466baa657b",
+    "url": "/cinema-/static/js/runtime~main.72601ca0.js"
+  },
+  {
+    "revision": "6eca75263898d58a12e5",
+    "url": "/cinema-/static/js/main.6eca7526.chunk.js"
+  },
+  {
+    "revision": "87fdc46663230b50969e",
+    "url": "/cinema-/static/js/1.87fdc466.chunk.js"
+  },
+  {
+    "revision": "6eca75263898d58a12e5",
+    "url": "/cinema-/static/css/main.f94e349c.chunk.css"
+  },
+  {
+    "revision": "c996dd74243c15f94e56221a63154fd1",
+    "url": "/cinema-/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/cinema-/precache-manifest.4ec7ef2758d204b0ee3255f39e57795e.js"
+  "/cinema-/precache-manifest.761f91428529817a373bdb33a723f75f.js"
 );
 
 workbox.clientsClaim();
